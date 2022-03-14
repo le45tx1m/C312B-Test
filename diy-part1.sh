194c84c99be48ca5ff2f40c36a12e3219f03e0c8
zfdx123 <2915441170@qq.com>
2022-03-14 08:27:23 +0800

    luci-app-control-webrestriction luci-app-control-weburl

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git OpenAppFilter https://github.com/destan19/OpenAppFilter' >> feeds.conf.default
+echo 'src-git extrapackages https://github.com/plasmolysis369/lede-extrapackages' >>feeds.conf.default
