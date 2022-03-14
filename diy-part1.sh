e0fa14df89aeef66cffd0dc31d40d29bd79ebfdb
zfdx123 <2915441170@qq.com>
2022-03-14 11:12:30 +0800

    Test

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git extrapackages https://github.com/plasmolysis369/lede-extrapackages' >>feeds.conf.default
+#echo 'src-git extrapackages https://github.com/plasmolysis369/lede-extrapackages' >>feeds.conf.default
+echo "src-git openappfilter https://github.com/destan19/OpenAppFilter" >> feeds.conf.default
